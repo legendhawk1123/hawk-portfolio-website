3e6050633ea412ee4f547767c4118a6e8897edf1
legendhawk1123 <zhenyu.li84822@gmail.com>
2024-04-22 21:12:47 -0600

    push for lint error

--- components/experience.tsx
+++ components/experience.tsx
@@ -37,6 +37,7 @@ export default function Experience() {
                     : "0.4rem solid rgba(255, 255, 255, 0.5)",
               }}
               date={item.date}
+              // @ts-ignore
               icon={item.icon}
               iconStyle={{
                 background:
